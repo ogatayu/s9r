62df934aea8e8a9ff9c4fe0aa79a6d7718cc7664
Yuya Ogata <y3o4xx@gmail.com>
2021-08-26 10:38:09 +0900

    (wip) add VCO

---
+++ s9r/src/VCO.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+
+class VCO {
+public:
+    VCO()
+    {
+        phase_       = 0;
+        nn_          = 0;
+        current_nn_  = 0;
+        detune_cent_ = 0;
+    }
+    ~VCO() {}
+
+    // variable
+    uint32_t phase_;        // 16:16 fixed-point
+    int      nn_;           // 発振指定されたノートNo
+    int      current_nn_;   // 現在発振中のノートNo
+    int      detune_cent_;  // ボイス間デチューン値（単位はセント）
+
+    void  setNoteNo(int nn, bool is_key_on)
+    {
+
+    }
+
+    float calc()
+    {
+        return 0.0f;
+    }
+};
